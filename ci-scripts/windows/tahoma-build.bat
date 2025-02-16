9265541214b71723b072a151ff2f166ab14b0228
manongjohn <19245851+manongjohn@users.noreply.github.com>
2025-02-16 11:19:13 -0500

    Update Windows build scripts for Qt6

--- ci-scripts/windows/tahoma-build.bat
+++ ci-scripts/windows/tahoma-build.bat
@@ -13,19 +13,14 @@ REM Setup for local builds
 set MSVCVERSION="Visual Studio 16 2019"
 set BOOST_ROOT=C:\boost\boost_1_74_0
 set OPENCV_DIR=C:\opencv\451\build
-REM set QT_PATH=C:\Qt\5.9.7\msvc2019_64
-set QT_PATH=C:\Qt\5.15.2_wintab\msvc2019_64
+set QT_PATH=C:\Qt\6.7.3\msvc2019_64
 
 REM These are effective when running from Actions
 IF EXIST C:\local\boost_1_74_0 set BOOST_ROOT=C:\local\boost_1_74_0
 IF EXIST C:\tools\opencv set OPENCV_DIR=C:\tools\opencv\build
-REM IF EXIST D:\a\tahoma2d\tahoma2d\thirdparty\qt\5.9\msvc2019_64 (
-REM 	set QT_PATH=D:\a\tahoma2d\tahoma2d\thirdparty\qt\5.9\msvc2019_64
-REM )
 
-set WITH_WINTAB=Y
-IF EXIST D:\a\tahoma2d\tahoma2d\thirdparty\qt\5.15.2_wintab\msvc2019_64 (
-	set QT_PATH=D:\a\tahoma2d\tahoma2d\thirdparty\qt\5.15.2_wintab\msvc2019_64
+IF EXIST D:\a\tahoma2d\tahoma2d\thirdparty\qt\6.7.3\msvc2019_64 (
+	set QT_PATH=D:\a\tahoma2d\tahoma2d\thirdparty\qt\6.7.3\msvc2019_64
 )
 
 set WITH_CANON=N
@@ -34,7 +29,7 @@ IF EXIST ..\..\thirdparty\canon\Header set WITH_CANON=Y
 set WITH_GPHOTO2=N
 IF EXIST ..\..\thirdparty\libgphoto2\include set WITH_GPHOTO2=Y
 
-cmake ..\sources -G %MSVCVERSION%  -Ax64 -DQT_PATH=%QT_PATH% -DBOOST_ROOT=%BOOST_ROOT% -DOpenCV_DIR=%OPENCV_DIR% -DWITH_CANON=%WITH_CANON% -DWITH_GPHOTO2=%WITH_GPHOTO2% -DWITH_WINTAB=%WITH_WINTAB%
+cmake ..\sources -G %MSVCVERSION%  -Ax64 -DQT_PATH=%QT_PATH% -DBOOST_ROOT=%BOOST_ROOT% -DOpenCV_DIR=%OPENCV_DIR% -DWITH_CANON=%WITH_CANON% -DWITH_GPHOTO2=%WITH_GPHOTO2%
 
 
 IF EXIST C:\ProgramData\chocolatey\bin\cl.exe (
